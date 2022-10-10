3fb50a3889608874ab96b6c476deddc9675e4fae
“firstnapat” <napatdmsu@gmail.com>
2022-10-10 23:18:22 +0700

    init ios project

---
+++ first-ios-app/first-ios-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  first-ios-app
+//
+//  Created by Napatsakorn Pianchana on 10/10/2565 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
